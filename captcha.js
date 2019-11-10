a360ed862db00989a7be81cbd108b417a8b6ef0a
Andrew <healeycodes@gmail.com>
2019-11-10 21:00:12 +0000

    Rearrange functions into the order they are called

--- captcha.js
+++ captcha.js
@@ -3,18 +3,18 @@ const { createCanvas } = require("canvas");
 const FONTBASE = 200;
 const FONTSIZE = 35;
 
-// Get a float between min and max
-const arbitraryRandom = (min, max) => Math.random() * (max - min) + min;
-
-// Get a rotation between -degrees and degrees converted to radians
-const randomRotation = (degrees = 15) => (arbitraryRandom(-degrees, degrees) * Math.PI) / 180;
-
 // Get a font size relative to base size and canvas width
 const relativeFont = width => {
   const ratio = FONTSIZE / FONTBASE;
   const size = width * ratio;
   return `${size}px serif`;
 };
+
+// Get a float between min and max
+const arbitraryRandom = (min, max) => Math.random() * (max - min) + min;
+
+// Get a rotation between -degrees and degrees converted to radians
+const randomRotation = (degrees = 15) => (arbitraryRandom(-degrees, degrees) * Math.PI) / 180;
 
 // https://gist.github.com/wesbos/1bb53baf84f6f58080548867290ac2b5
 const alternateCapitals = str =>
