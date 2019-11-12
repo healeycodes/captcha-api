83d05fc2776d571b49cc230d3d386dd0b0573a7a
Andrew <healeycodes@gmail.com>
2019-11-12 19:34:17 +0000

    Refactor tests to include checking validation string

--- test/app.test.js
+++ test/app.test.js
@@ -7,22 +7,24 @@ const app = require("../app");
 
 describe("captcha", () => {
   describe("testing captcha default", () => {
-    it("should respond with a valid data URL", async () => {
-      const image = await request(app)
+    it("should respond with a valid data URL and a validation string", async () => {
+      const body = await request(app)
         .get("/captcha")
         .expect(200)
-        .then(res => res.body.image);
-      assert(validDataURL(image));
+        .then(res => res.body);
+      assert(validDataURL(body.image));
+      assert(typeof body.text === "string" && body.text.length > 0);
     });
   });
 
   describe("testing captcha default with custom params", () => {
-    it("should respond with a valid data URL", async () => {
-      const image = await request(app)
+    it("should respond with a valid data URL and a validation string", async () => {
+      const body = await request(app)
         .get("/captcha/300/150")
         .expect(200)
-        .then(res => res.body.image);
-      assert(validDataURL(image));
+        .then(res => res.body);
+      assert(validDataURL(body.image));
+      assert(typeof body.text === "string" && body.text.length > 0);
     });
   });
 });
